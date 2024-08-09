e68a1d20fd27aba829e31da8587a1545be762b96
ScaredTerryCat <mariohutu115@gmail.com>
2024-08-09 05:36:01 -0700

    Delete file2.php

--- file2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username=$_POST["username"];
-$password=$_POST["password"];
-$file=fopen("file5.txt","a");
-$content="username: $username | password: $password";
-fwrite($file,$content);
-?>
